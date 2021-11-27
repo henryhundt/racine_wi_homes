4de6cbb3721182dc6fb2d665a19f92a8ef700d33
Henry Hundt <33141189+henryhundt@users.noreply.github.com>
2021-11-27 16:22:29 -0600

    Cleaned up.

--- src/Racine.py
+++ src/Racine.py
@@ -1,23 +1,17 @@
-# This script opens and pulls all census data related to Racine, WI from Morgan Edward's cleaned and combined census df
+# This script opens and pulls all data related to Racine, WI from MEdward's cleaned Zillow data.
 
 import pandas as pd
 import numpy as np
 
-#read in cansus data
-#census_csv = 'df_census.csv'
 
+#read in zillow data sort for City: Racine, State: WI
 zillow = pd.read_csv('Data/df_zillow.csv')
 cond_ = (zillow["City"] == 'RACINE') & (zillow["State"] == 'WI')
 zillow.loc[cond_]
+
+#new df for Racine WI
 racine_wi = zillow.loc[cond_]
 
+#export to CSV file
 racine_wi.to_csv('Data/racine_wi.csv', index=False)
 
-#learning process
-#zillow_smalltest = zillow.head(1000)
-#zillow_smalltest.dtypes
-#print(zillow_smalltest[zillow_smalltest.City == 'MARBURY'])
-#marbury = zillow_smalltest[zillow_smalltest.City == 'MARBURY']
-#marbury_gv = marbury[marbury.HeatingType == 'GV']
-
-#arbury_gv.to_csv('data/df_marbury_gv.csv', index=False)
